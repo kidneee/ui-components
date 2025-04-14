fed67f8b583685231ee05a35bbb537a242b1d3b4
kido <skido627@gmail.com>
2025-04-14 15:12:48 +0900

    Update: 開閉時のボタンの挙動を追加

--- accordion-menu/script.js
+++ accordion-menu/script.js
@@ -3,9 +3,16 @@ const faqItemContents = document.querySelectorAll('.faq__item-content');
 
 faqItemButtons.forEach(function (faqItemButton, index) {
   faqItemButton.addEventListener('click', () => {
+    let isOpen = faqItemContents[index].classList.contains('is-active');
+
     faqItemContents.forEach(faqItemContent => {
+      faqItemButton.classList.remove('is-active');
       faqItemContent.classList.remove('is-active');
     });
-    faqItemContents[index].classList.add('is-active');
+
+    if (!isOpen) {
+      faqItemButtons[index].classList.add('is-active');
+      faqItemContents[index].classList.add('is-active');
+    }
   })
 });
